5f51dd3229dec90b6d9c57a5ec4ba85c26dec572
WasefUllah <wasefudvash@gmail.com>
2025-06-14 00:53:05 +0600

    added all marathon list

--- src/Components/Marathon/Marathon.jsx
+++ src/Components/Marathon/Marathon.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import MarathonCard from "../MarathonCard/MarathonCard";
+import { format } from "date-fns";
+import MakeBlurText from "../MakeBlurText.jsx/MakeBlurText";
+import { Link } from "react-router";
 
 const Marathon = () => {
-    return (
-        <div>
-            this is marathon
+  const [marathons, setMarathons] = useState([]);
+
+  useEffect(() => {
+    axios("http://localhost:3000/allmarathons")
+      .then((res) => setMarathons(res.data))
+      .catch((err) => console.log(err));
+  }, []); // empty dependency array = run once when component mounts
+
+  console.log(marathons);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+      {marathons.map((marathon) => (
+        <div
+          key={marathon._id}
+          className="p-2 min-h-[450px] flex flex-col space-y-1 justify-center"
+        >
+          <div className="mb-2 ">
+            <img
+              src={marathon.photo}
+              alt=""
+              className="w-full h-74 object-center rounded-2xl"
+            />
+          </div>
+
+          <div className="text-primary text-lg md:text-xl lg:text-2xl">
+            <MakeBlurText text={marathon.title}></MakeBlurText>
+          </div>
+          <div className="text-gray-400 text-xs md:text-md lg:text-lg">
+            <MakeBlurText text={marathon.location}></MakeBlurText>
+            <MakeBlurText
+              text={`Registration date: ${format(
+                marathon.startRegDate,
+                "do MMMM, yyyy"
+              )}`}
+            ></MakeBlurText>
+          </div>
+          <div className="flex justify-end">
+            <Link to={`/marathons/${marathon._id}`}>
+              <button className="btn-sm md:btn lg:btn-lg btn-primary">
+                See details
+              </button>
+            </Link>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Marathon;
--- src/Components/MyMarathonList/MyMarathonList.jsx
+++ src/Components/MyMarathonList/MyMarathonList.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const MyMarathonList = () => {
-    return (
-        <div>
-            My marathon list
-        </div>
-    );
+  const [marathons, setMarathons] = useState([]);
+  const { user } = useContext(AuthContext);
+  axios(`http://localhost:3000/allmarathons?email=${user.email}`)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  return <div>My marathon list</div>;
 };
 
 export default MyMarathonList;
