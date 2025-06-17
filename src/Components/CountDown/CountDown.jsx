e00910c1ce3c0e19919c4b46320465c32caefe72
WasefUllah <wasefudvash@gmail.com>
2025-06-17 23:52:25 +0600

    fixed count down responsiveness

--- src/Components/CountDown/CountDown.jsx
+++ src/Components/CountDown/CountDown.jsx
@@ -37,7 +37,7 @@ const CountDown = ({ endTime }) => {
   const daysDuration = days * daySeconds;
   return (
     <div className="App flex flex-col lg:flex-row items-center justify-between">
-     <div className="flex justify-center items-center gap-1">
+     <div className="sm:flex sm:justify-between sm:items-center gap-2 lg:gap-60">
        <CountdownCircleTimer
         {...timerProps}
         colors="#7E2E84"
@@ -67,7 +67,7 @@ const CountDown = ({ endTime }) => {
       </CountdownCircleTimer>
      </div>
 
-      <div className="flex justify-center items-center gap-1">
+      <div className="sm:flex sm:justify-between sm:items-center gap-2 lg:gap-60">
         <CountdownCircleTimer
         {...timerProps}
         colors="#EF798A"
